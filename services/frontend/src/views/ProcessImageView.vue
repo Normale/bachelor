06d92e08d8eede9e61be74e26c9391ad3341871f
Normale <52136684+Normale@users.noreply.github.com>
2023-04-20 13:45:43 +0200

    Update ProcessImageView.vue

--- services/frontend/src/views/ProcessImageView.vue
+++ services/frontend/src/views/ProcessImageView.vue
@@ -1,26 +1,30 @@
+<!-- src/views/ProcessImage.vue -->
 <template>
     <div>
       <h1>Process Image</h1>
       <div v-if="selectedImage">
-        <div>
-            <label>
+        <div class="options">
+          <label>
             <input
-            type="radio"
-            id="optionA"
-            value="a"
-            v-model="selectedOption"
+              type="radio"
+              id="optionA"
+              value="a"
+              v-model="selectedOption"
             />Option A
-        </label>
-        <label>
+          </label>
+          <label>
             <input
-            type="radio"
-            id="optionB"
-            value="b"
-            v-model="selectedOption"
+              type="radio"
+              id="optionB"
+              value="b"
+              v-model="selectedOption"
             />Option B
-        </label>
-    </div>
-    <img :src="selectedImage" alt="Selected image" class="processed-image" />
+          </label>
+        </div>
+        <img :src="selectedImage" alt="Selected image" class="processed-image" />
+        <div class="generate-btn-container">
+          <button @click="generate" class="generate-btn">Generate</button>
+        </div>
       </div>
       <div v-else>
         <p>No image selected. Please go back and select an image.</p>
@@ -28,29 +32,66 @@
     </div>
   </template>
   
-<script>
-import { defineComponent } from 'vue';
-import { mapGetters } from 'vuex';
-
-export default defineComponent({
+  <script>
+  import { defineComponent } from 'vue';
+  import { mapGetters } from 'vuex';
+  
+  export default defineComponent({
     name: 'ProcessImageView',
     computed: {
-        ...mapGetters('images', ['selectedImage']),
+      ...mapGetters('images', ['selectedImage']),
     },
     data() {
-        return {
-            selectedOption: 'a',
-        };
+      return {
+        selectedOption: 'a',
+      };
     },
-});
-</script>
+    methods: {
+      generate() {
+        console.log('Generate button clicked');
+        // Add your generate functionality here
+      },
+    },
+  });
+  </script>
   
-<style scoped>
-.processed-image {
+  <style scoped>
+  .processed-image {
     max-width: 100%;
     max-height: 300px;
     display: block;
     margin-bottom: 1rem;
-}
-</style>
+  }
+  
+  .options {
+    display: flex;
+    gap: 1rem;
+    justify-content: center;
+    margin-bottom: 1rem;
+  }
+  
+  .options label {
+    cursor: pointer;
+  }
+  
+  .generate-btn-container {
+    display: flex;
+    justify-content: flex-end;
+  }
+  
+  .generate-btn {
+    background-color: #007bff;
+    color: #fff;
+    border: none;
+    padding: 0.5rem 1rem;
+    font-size: 1rem;
+    cursor: pointer;
+    border-radius: 0.25rem;
+    transition: background-color 0.2s;
+  }
+  
+  .generate-btn:hover {
+    background-color: #0056b3;
+  }
+  </style>
   
