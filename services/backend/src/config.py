9baa94e326d260ce42ed9c74ecb36090d185738a
Normale <52136684+Normale@users.noreply.github.com>
2023-04-20 12:07:01 +0200

    Fix base_image_url

--- services/backend/src/config.py
+++ services/backend/src/config.py
@@ -3,7 +3,7 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     IMAGE_UPLOAD_DIR: str = "storage/images"
-    BASE_IMAGE_URL: str = "http://localhost:8000/images"
+    BASE_IMAGE_URL: str = "http://localhost:5000/images"
 
 
 settings = Settings()
