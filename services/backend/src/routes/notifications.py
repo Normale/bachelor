8e449f07702594643a6b6a674e29dc3dea6763e2
Normale <52136684+Normale@users.noreply.github.com>
2023-08-06 01:41:43 +0200

    Add data to notifications

--- services/backend/src/routes/notifications.py
+++ services/backend/src/routes/notifications.py
@@ -2,11 +2,13 @@ from fastapi import APIRouter, Depends
 from src.auth.jwthandler import get_current_user
 import asyncio
 import aiopg
+import json
+
 
 router = APIRouter(tags=["Notifications"])
 
 @router.get("/listen-for-changes")
-async def listen_for_changes():
+async def listen_for_changes(id: int):
     # Connection details - replace with your actual details
     dsn = 'dbname=bachelor_db user=bachelor password=bachelor host=db port=5432'
 
@@ -14,7 +16,8 @@ async def listen_for_changes():
         async with conn.cursor() as cur:
             await cur.execute("LISTEN results;")
             msg = await asyncio.wait_for(conn.notifies.get(), timeout=10)
-            print(f"Received notification: {msg.payload}")
+            payload_data = json.loads(msg.payload)  # Parse the payload as JSON
+            print(f"Received notification: {payload_data}")
     
     # Return a simple message. You might want to return something more meaningful in your application.
     return {"message": "Received notification"}
